1d17a858a97d7ce482c5395da47f9754d2a0c4c9
YiHyeongRae <dev.yihr@gmail.com>
2023-04-18 16:06:53 +0900

    add:addToDo API 추가

--- src/pages/api/todo/addToDo.ts
+++ src/pages/api/todo/addToDo.ts
@@ -1,17 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import getInsertValues from "../../../../util/getInsertValues";
 const db = require("../../../../common/config/db");
 
 export default async function addToDo(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // db.query(`SELECT * FROM op_user`, function (err: any, result: any) {
-  //   if (err) {
-  //     // console.log("에러는 ?", err);
-  //     res.send(err);
-  //   } else {
-  //     // console.log("결과는 ?", result);
-  //     res.json(result);
-  //   }
-  // });
+  // console.log("body.data", req.body.data);
+  const values = getInsertValues(req.body.data);
+
+  db.query(
+    `INSERT INTO op_todo (name,quantity,price) VALUES${values}`,
+    function (err: any, result: any) {
+      if (err) {
+        // console.log("에러는 ?", err);
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
 }

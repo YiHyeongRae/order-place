65956c605ae93c264dbe2ba31dfa085a54d86dc7
YiHyeongRae <dev.yihr@gmail.com>
2023-04-20 14:13:40 +0900

    add: footer 추가

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { Component, PropsWithChildren, ReactElement } from "react";
 import styled from "styled-components";
+import Footer from "./Footer";
 import Header from "./Header";
 
 const LayoutWrap = styled.div`
@@ -19,6 +20,7 @@ function Layout(props: Props) {
     <LayoutWrap>
       <Header />
       <ChildrenWrap>{props.children}</ChildrenWrap>
+      <Footer />
     </LayoutWrap>
   );
 }

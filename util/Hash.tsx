6647102c5c4f6bd29277634eaf099afb41424059
YiHyeongRae <dev.yihr@gmail.com>
2023-04-21 10:49:13 +0900

    add: hashing util 함수 추가

---
+++ util/Hash.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import SHA256 from "crypto-js/sha256";
+
+import crypto from "crypto";
+interface HashModuels {
+  makeHash: Function;
+}
+const Hash: HashModuels = {
+  makeHash: async (userId: string, userPw: string) => {
+    console.log("hash에 들어오는 id", userId);
+    const res = await axios.post("http://localhost:3000/api/user/getSalt", {
+      id: userId,
+    });
+    console.log("hash res", res);
+    const stretching = 1000;
+    if (res.data.length !== 0) {
+      const salt = res.data[0].salt;
+      return new Promise(async (resolve, reject) => {
+        crypto.pbkdf2(
+          userPw,
+          salt,
+          stretching,
+          64,
+          "sha512",
+          (err: any, key: any) => {
+            if (err) reject(err);
+            resolve(key.toString("base64"));
+          }
+        );
+      });
+    } else {
+      return null;
+    }
+  },
+};
+
+export default Hash;

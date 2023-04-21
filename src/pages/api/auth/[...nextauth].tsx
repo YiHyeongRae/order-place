2a2244092b38ac2fe9814dc77468ddb6631ed1cc
YiHyeongRae <dev.yihr@gmail.com>
2023-04-21 10:49:36 +0900

    feat: 로그인 로직 추가

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { getSession } from "next-auth/react";
-// import Hash from "../../../lib/Hash";
+import Hash from "../../../../util/Hash";
 
 interface userResData {
   user: {
@@ -17,27 +17,27 @@ export const authOptions = {
   providers: [
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
+      id: "login",
+      name: "login",
       // `credentials` is used to generate a form on the sign in page.
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
+      credentials: {},
+      async authorize(req: any) {
+        // console.log("credentials,req", req);
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
 
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
+        const res = await axios.post("http://localhost:3000/api/user/login", {
+          data: { id: req.userId, pw: req.hashedPw },
+        });
+        console.log("nextAuth", res.data);
+
+        if (res.data.length === 0) {
           return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        } else {
+          const user = { id: res.data.user_id, name: res.data.name };
+          return user;
         }
       },
     }),
@@ -56,43 +56,24 @@ export const authOptions = {
   },
   callbacks: {
     async signIn({ user, account, profile, email, credentials }: any) {
-      console.log(user, account, profile, email, credentials);
+      // console.log(user, account, profile, email, credentials);
       // credential provider에서 return 해준 user 찍힘
-      // user{
-      //  id: 'admin',
-      //  name: '이형래',
-      //  authority: 'all',
-      //  email: 'user@test.com',
-      //  picture: 'user.jpeg'
-      //  image:'userImage.jpeg'
-      // }
+
       return true;
     },
     async redirect({ url, baseUrl }: any) {
-      // console.log("url,baseUrl", url, baseUrl);
       // Allows relative callback URLs
-      //if (url.startsWith("/")) return `${baseUrl}${url}`;
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
       // Allows callback URLs on the same origin
-      //else if (new URL(url).origin === baseUrl) return url;
+      else if (new URL(url).origin === baseUrl) return url;
       return baseUrl;
     },
     async jwt({ token, account, user }: any) {
       if (account) {
         token.accessToken = account.access_token;
         // token 반환값
-        // token{
-        //  name: '이형래',
-        //  email: 'user@test.com',
-        //  picture: 'userImage.jpeg',
-        //  sub: 'admin'
-        // }
+
         // 필요시 토큰 수정 가능
-        // token.mobile = "010-1234-5678";
-        // token.test = "this is test";
-        // token.email = "token@test.com";
-        // token.picture = "token.jpeg";
-        // token.authority = user.authority;
-        // token.accessToken = account.access_token;
       }
       return token;
     },
@@ -113,37 +94,6 @@ export const authOptions = {
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
-};
-
-const login: Function = async (
-  userId: string | undefined,
-  userPw: string | undefined
-) => {
-  //console.log("login id/pw ======", userId, userPw);
-
-  const res = await axios.post(
-    process.env.NEXT_PUBLIC_ORIGIN_HOST + "/api/user/login",
-    {
-      loginData: {
-        id: userId,
-        pw: userPw,
-      },
-    }
-  );
-
-  // const res = await axios.post("http://localhost:3000/api/user/login", {
-  //   loginData: {
-  //     id: userId,
-  //     pw: userPw,
-  //   },
-  // });
-  // console.log("로그인 에러 체크 [nextAuth]-login func", res);
-  if (res.data.loginState === true) {
-    // console.log("res가 찍히나 ?", res.data);
-    return res.data;
-  } else {
-    return alert("비밀번호 또는 아이디가 틀렸습니다"), res.data.error;
-  }
 };
 
 export default NextAuth(authOptions);
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+import { redirect } from "next/dist/server/api-utils";
+import Hash from "../../util/Hash";
+import { useRouter } from "next/router";
 
 const LoginWrap = styled.div`
   display: flex;
@@ -7,7 +12,7 @@ const LoginWrap = styled.div`
   align-items: center;
   flex-direction: column;
   gap: 16px;
-  min-height: calc(100vh - 99px);
+  min-height: calc(100vh - 130px);
 `;
 const CircleButtons = styled.button`
   /* background-color: #fdec; */
@@ -31,6 +36,8 @@ const Inputs = styled.input`
   outline: none;
   padding: 8px 0;
   text-indent: 8px;
+  font-size: 10px;
+  width: 100%;
 `;
 const ButtonWrap = styled.div`
   display: flex;
@@ -39,21 +46,57 @@ const ButtonWrap = styled.div`
   padding-top: 8px;
 `;
 
-function login() {
+function Login() {
+  const router = useRouter();
+  const [userId, setUserId] = useState<string>();
+  const [userPw, setUserPw] = useState<string>();
+  const { data: session } = useSession();
+  // console.log("id,pw", userId, userPw);
+  console.log("session", session);
+
+  const login: Function = async () => {
+    const hashedPw = await Hash.makeHash(userId, userPw)
+      .then((hashedPw: string) =>
+        signIn("login", { userId, hashedPw, redirect: false })
+      )
+      .then((result: any) => {
+        console.log("result", result);
+        if (result.status === 200) {
+          router.replace("/");
+        } else {
+          alert("아이디 혹은 비밀번호가 틀렸습니다.");
+        }
+      });
+  };
   return (
     <LoginWrap>
       <div>LOGIN</div>
       <LoginForm>
-        <Inputs type="text" placeholder="ID" />
+        <Inputs
+          type="text"
+          placeholder="ID"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setUserId(e.currentTarget.value)
+          }
+        />
 
-        <Inputs type="paswword" placeholder="PASSWORD" />
+        <Inputs
+          type="password"
+          placeholder="PASSWORD"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setUserPw(e.currentTarget.value)
+          }
+        />
 
         <ButtonWrap>
-          <CircleButtons type="button">로그인</CircleButtons>
+          <CircleButtons type="button" onClick={() => login()}>
+            로그인
+          </CircleButtons>
         </ButtonWrap>
       </LoginForm>
+      <div>회원가입은 관리자에게 문의해주세요.</div>
     </LoginWrap>
   );
 }
 
-export default login;
+export default Login;

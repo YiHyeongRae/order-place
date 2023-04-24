c37f44e719b987904746c2e37e7e021420c4e114
YiHyeongRae <dev.yihr@gmail.com>
2023-04-24 18:01:40 +0900

    add: LoadingSpinner 추가

---
+++ components/LoadingSpinner.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+const Spinner = styled.div`
+  width: 36px;
+  height: 36px;
+  /* border: 4px solid rgba(232, 235, 237, 0.5);
+  border-top: 4px solid rgb(100, 47, 250); */
+  border-radius: 50%;
+  position: absolute;
+  z-index: 500;
+  left: 50%;
+  top: 50%;
+  animation: spin 0.75s linear infinite;
+  @keyframes spin {
+    0% {
+      transform: translate(-50%, -50%) rotateY(0deg);
+    }
+    100% {
+      transform: translate(-50%, -50%) rotateY(360deg);
+    }
+  }
+`;
+
+const SpinnerText = styled.div`
+  margin-top: 36px;
+  position: absolute;
+  z-index: 500;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+function LoadingSpinner() {
+  // 스크롤 막기
+
+  return (
+    <div className="loading-spinner">
+      <Spinner>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="#f55353"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          style={{ width: 36, color: "#f55353" }}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+          />
+        </svg>
+      </Spinner>
+      <SpinnerText>Loading</SpinnerText>
+    </div>
+  );
+}
+
+export default LoadingSpinner;

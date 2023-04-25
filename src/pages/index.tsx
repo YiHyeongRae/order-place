b8b55bf6f22744883e787b1d4b246a8ca7579bac
YiHyeongRae <dev.yihr@gmail.com>
2023-04-25 17:00:28 +0900

    feat: Check 컴포넌트로 분리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import useSWR, { useSWRConfig } from "swr";
+import Check from "../../components/Check";
 
 import { fetcher } from "../../util/fetcher";
 import getTodayDate from "../../util/getTodayDate";
@@ -21,11 +22,6 @@ const CheckWrap = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 16px;
-`;
-
-const CheckMarker = styled.p`
-  display: flex;
-  align-items: center;
 `;
 
 const CheckItem = styled.li`
@@ -557,78 +553,7 @@ export default function Home({ CCC, CDC, CBB, Category, user }: any) {
         <></>
       )}
       {/* Check */}
-      <Content>
-        <Title>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            style={{ width: "20px", color: "#000" }}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12"
-            />
-          </svg>
-
-          <p>Check</p>
-        </Title>
-
-        <CheckWrap>
-          <li style={{ textAlign: "center" }}>
-            통계 데이터가 쌓일 때 까지 조금만 기다려주세요!
-          </li>
-          {/* <CheckItem>
-            <CheckMarker>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                style={{ width: "20px", color: "red" }}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-                />
-              </svg>
-            </CheckMarker>
-
-            <p>
-              &quot;서울)생크림&quot; 을 발주하신지 14일이 지났어요!
-              발주&amp;재고 확인을 추천드립니다!
-            </p>
-          </CheckItem>
-          <CheckItem>
-            <CheckMarker>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                style={{ width: "20px", color: "red" }}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-                />
-              </svg>
-            </CheckMarker>
-
-            <p>
-              &quot;서울)생크림&quot; 을 발주하신지 14일이 지났어요!
-              발주&amp;재고 확인을 추천드립니다!
-            </p>
-          </CheckItem> */}
-        </CheckWrap>
-      </Content>
+      <Check />
       {/* ToDo */}
       <Content>
         <Title>

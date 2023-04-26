063e46e545950ce846970546e933ae32e2f22df3
YiHyeongRae <dev.yihr@gmail.com>
2023-04-26 16:50:35 +0900

    feat: util 함수 user_id 추가

--- util/getInsertValues.tsx
+++ util/getInsertValues.tsx
@@ -1,15 +1,16 @@
 function getInsertValues(data: any, date?: any) {
   const arr: any = [];
-
   if (date) {
     data.map((item: any) => {
       return arr.push(
-        `("${item.name}","${item.quantity}","${item.price}","${date}")`
+        `("${item.name}","${item.quantity}","${item.price}","${date}","${item.user_id}","n")`
       );
     });
   } else {
     data.map((item: any) => {
-      return arr.push(`("${item.name}","${item.quantity}","${item.price}")`);
+      return arr.push(
+        `("${item.name}","${item.quantity}","${item.price}","${item.user_id}")`
+      );
     });
   }
 

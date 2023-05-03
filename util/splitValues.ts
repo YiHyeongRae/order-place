e7bb3e1426e55df2469aa7c7a4c8eb6cb09b7d4c
YiHyeongRae <dev.yihr@gmail.com>
2023-05-03 16:21:40 +0900

    fix: util 함수 data props 추가

--- util/splitValues.ts
+++ util/splitValues.ts
@@ -1,29 +1,29 @@
 import React from "react";
 
-async function splitValues() {
-  const testArr = [
-    {
-      data1: "1",
-      data2: "원",
-      data3: "하나",
-    },
-    {
-      data1: "2",
-      data2: "투",
-      data3: "둘",
-    },
-    {
-      data1: "3",
-      data2: "쓰리",
-      data3: "삼",
-    },
-  ];
+async function splitValues(data: any) {
+  // const data = [
+  //   {
+  //     data1: "1",
+  //     data2: "원",
+  //     data3: "하나",
+  //   },
+  //   {
+  //     data1: "2",
+  //     data2: "투",
+  //     data3: "둘",
+  //   },
+  //   {
+  //     data1: "3",
+  //     data2: "쓰리",
+  //     data3: "삼",
+  //   },
+  // ];
 
-  const objLength = Object.keys(testArr[0]).length;
+  const objLength = Object.keys(data[0]).length;
 
   const valueString: any = [];
 
-  testArr.map((item: any) => {
+  data.map((item: any) => {
     const thisArr: any = [];
     for (let i = 0; i < objLength; i++) {
       thisArr.push(`"${Object.values(item)[i]}"`);
@@ -32,6 +32,8 @@ async function splitValues() {
     valueString.push(completeValue);
   });
   console.log("test", valueString);
+
+  return valueString;
 }
 
 export default splitValues;

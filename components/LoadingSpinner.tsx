8516cf66ab28509186160f1e0f684c7b47c26dcf
YiHyeongRae <dev.yihr@gmail.com>
2023-05-04 15:52:31 +0900

    feat: LoadingSpinner props 추가

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
+const SpinnerWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  /* background-color: rgba(0, 0, 0, 0.7); */
+`;
+
 const Spinner = styled.div`
   width: 36px;
   height: 36px;
@@ -12,6 +21,7 @@ const Spinner = styled.div`
   left: 50%;
   top: 50%;
   animation: spin 0.75s linear infinite;
+
   @keyframes spin {
     0% {
       transform: translate(-50%, -50%) rotateY(0deg);
@@ -30,29 +40,57 @@ const SpinnerText = styled.div`
   top: 50%;
   transform: translate(-50%, -50%);
 `;
-function LoadingSpinner() {
+
+interface LoadingSpinnerTypes {
+  blur?: boolean;
+}
+function LoadingSpinner({ blur }: LoadingSpinnerTypes) {
   // 스크롤 막기
 
   return (
-    <div className="loading-spinner">
-      <Spinner>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="#f55353"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          style={{ width: 36, color: "#f55353" }}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-          />
-        </svg>
-      </Spinner>
-      <SpinnerText>Loading</SpinnerText>
-    </div>
+    <>
+      {blur ? (
+        <SpinnerWrapper className="loading-spinner">
+          <Spinner>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#f55353"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              style={{ width: 36, color: "#f55353" }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </svg>
+          </Spinner>
+          <SpinnerText>Loading</SpinnerText>
+        </SpinnerWrapper>
+      ) : (
+        <>
+          <Spinner>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#f55353"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              style={{ width: 36, color: "#f55353" }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </svg>
+          </Spinner>
+          <SpinnerText>Loading</SpinnerText>
+        </>
+      )}
+    </>
   );
 }
 
